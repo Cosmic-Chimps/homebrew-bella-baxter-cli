a169c18961af7bada5fafbd413c083ecfa7dc5f6
jjchiw <jjchiw@gmail.com>
2026-04-26 00:31:00 +0000

    chore: update bella formula to v0.1.1-preview.57

--- Formula/bella.rb
+++ Formula/bella.rb
@@ -7,30 +7,30 @@
 class Bella < Formula
   desc "Bella Baxter CLI — manage and consume secrets from Bella Baxter"
   homepage "https://bella-baxter.io"
-  version "0.1.1-preview.55"
+  version "0.1.1-preview.57"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.55/cli-osx-arm64"
-      sha256 "6ad49264d7bb107611b49868d80d7246d63c3856b1cbf83db97d9abee7f67a88"
+      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.57/cli-osx-arm64"
+      sha256 "0211d56c1accad894d116d9da72fbd3fc48efb1e4b3cc3b992f1b139ea074622"
     end
 
     on_intel do
-      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.55/cli-osx-x64"
-      sha256 "07fda7dd03a722c9da50a663e49e87b09aca8dcb95921ad771e391c21f569a6d"
+      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.57/cli-osx-x64"
+      sha256 "a74543ffac19b45761faa58e86ecc6781d76903c253a4bc82cffd828bf7f6003"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.55/cli-linux-arm64"
-      sha256 "65cc68220ca4d08f2d8132c44572615b7b0627473edf9b85bcb0f40d10c1c9f5"
+      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.57/cli-linux-arm64"
+      sha256 "03ee6b69f84143e8251efc4221c5c9bfa9ecd24c9a3472156d0340d99cf392f5"
     end
 
     on_intel do
-      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.55/cli-linux-x64"
-      sha256 "2e63ae858ee1e0caa14af00b7def01d8f1451e99216e50ba68e685f7610f1de3"
+      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.57/cli-linux-x64"
+      sha256 "d12b53ad886d5375a472b609e70cdf647b3a064901fa50e00c762c5181d2e2c1"
     end
   end
 
