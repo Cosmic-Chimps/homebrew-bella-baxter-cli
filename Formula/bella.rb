1ca0828411d61c99877df57bcabad5f2b7746382
jjchiw <jjchiw@gmail.com>
2026-09-22 17:19:06 +0000

    chore: update bella formula to v0.1.1-preview.114

--- Formula/bella.rb
+++ Formula/bella.rb
@@ -7,30 +7,30 @@
 class Bella < Formula
   desc "Bella Baxter CLI — manage and consume secrets from Bella Baxter"
   homepage "https://bella-baxter.io"
-  version "0.1.1-preview.113"
+  version "0.1.1-preview.114"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.113/cli-osx-arm64"
-      sha256 "26aab308c0308078a52019abe59e45c3dbd2b767c2dfe832e689d51dead13c89"
+      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.114/cli-osx-arm64"
+      sha256 "503eb5cbdb4e92c2a4d6eeb47009f93f052089b28dc077a220f402fe2f0cd599"
     end
 
     on_intel do
-      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.113/cli-osx-x64"
-      sha256 "c9270e7097fd414a4570a86b99707c04bea7c1a814464fb5a642d13f4c729f6f"
+      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.114/cli-osx-x64"
+      sha256 "5b70df2ea25d8d9019923bd9e3c2a11588041444de236ea65dad30866a8d92ff"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.113/cli-linux-arm64"
-      sha256 "fba8ac4994d8e624995100241c469871ae9e12c04a51c77dc0fe09453c660d2d"
+      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.114/cli-linux-arm64"
+      sha256 "2d7c235afa42e313d931ffdc798f972784f42ea93c2d9bbf63cfd35cb7bcc009"
     end
 
     on_intel do
-      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.113/cli-linux-x64"
-      sha256 "f39ac7d6886f35efe87b59bf20e6024fa2402a8c5c570cc93eb9347e62e82086"
+      url "https://github.com/cosmic-chimps/bella-baxter-cli/releases/download/v0.1.1-preview.114/cli-linux-x64"
+      sha256 "6e53b0ed0388a6ce824de1e87cbb1fce435ed4ce07bd6482173bc7cd332a4a6a"
     end
   end
 
